a3621b6e285d8210bf4a4574f7b747b0b9b064f3
kimwoojin <kimwoojin1903@gmail.com>
2023-11-07 23:28:46 +0900

    firebase 설정관련 코드 추가

--- public/app.js
+++ public/app.js
@@ -1,3 +1,25 @@
+
+function initializeApp() {
+  // Firebase 프로젝트 설정 객체
+  const firebaseConfig = {
+    apiKey: "AIzaSyBxyxuRlvxV3KsA4KErPwlZCPhbsHDN-IU",
+    authDomain: "ramen-log.firebaseapp.com",
+    projectId: "ramen-log",
+    storageBucket: "ramen-log.appspot.com",
+    messagingSenderId: "292296321273",
+    appId: "1:292296321273:web:bacc165856b927edff5a28",
+    measurementId: "G-LZHR47DK5H"
+  };
+}
+  // Firebase 초기화
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const database = firebase.database();
+  const firestore = firebase.firestore();
+
+
+
+
 function renderQuickSearchPage() {
   const root2 = document.getElementById('root2');
   root2.innerHTML = '<p>빠른 검색 페이지 내용</p>';
