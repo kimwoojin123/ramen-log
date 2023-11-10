2a2a5bf46a4225366a8d3c5a18748bc3e7b35f27
kimwoojin123 <kimwoojin1903@gmail.com>
2023-11-10 16:29:25 +0900

    서버 수정작업중

--- public/routes.js
+++ public/routes.js
@@ -1,21 +1,29 @@
-const router = new YourRoutingLibrary();
+document.addEventListener("DOMContentLoaded", function () {
+  const router = new Director();
 
-// 페이지 경로와 해당 페이지를 렌더링할 함수를 추가합니다.
-router.addRoute('#quick-search', () => {
-  renderQuickSearchPage();
+  // 페이지 경로와 해당 페이지를 렌더링할 함수를 추가합니다.
+  router.addRoute("#quick-search", () => {
+    renderQuickSearchPage();
+  });
+
+  router.addRoute("#advanced-search", () => {
+    renderAdvancedSearchPage();
+  });
+
+  router.addRoute("#favorites", () => {
+    renderFavoritesPage();
+  });
+
+  router.addRoute("#signup", () => {
+    renderSignupPage();
+  });
+
+  // 기본 라우트 설정 (해시값이 없는 경우 기본 페이지 설정)
+  router.setDefaultRoute("#quick-search");
+
+  router.handleRoute();
+
+  router.on("notfound", () => {
+    console.log("Page not found");
+  });
 });
-
-router.addRoute('#advanced-search', () => {
-  renderAdvancedSearchPage();
-});
-
-router.addRoute('#favorites', () => {
-  renderFavoritesPage();
-});
-
-router.addRoute('#signup', () => {
-  renderSignupPage();
-});
-
-// 기본 라우트 설정 (해시값이 없는 경우 기본 페이지 설정)
-router.setDefaultRoute('#quick-search');
