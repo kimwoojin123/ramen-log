7eb7bf8e746ac06f089accfd4eb45b6634ef2002
kimwoojin123 <kimwoojin1903@gmail.com>
2023-11-13 10:32:40 +0900

    코드 리팩토링

--- public/app.js
+++ public/app.js
@@ -1,10 +1,9 @@
-
-import './routes.js';
-import firebase from 'firebase/app.js';
-import 'firebase/auth';
-import 'firebase/database';
-import 'firebase/firestore';
-
+import "./routes.js";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/firestore";
+import "firebase/analytics"; // 필요한 모듈 추가
 
 const firebaseConfig = {
   apiKey: "AIzaSyBxyxuRlvxV3KsA4KErPwlZCPhbsHDN-IU",
@@ -16,6 +15,11 @@ const firebaseConfig = {
   measurementId: "G-LZHR47DK5H",
 };
 
+// 나머지 코드는 그대로 유지
+const auth = firebase.auth();
+const database = firebase.database();
+const firestore = firebase.firestore();
+
 // initializeApp 함수 정의
 export function initializeApp() {
   // Firebase 초기화
@@ -24,28 +28,3 @@ export function initializeApp() {
 
 // DOMContentLoaded 이벤트에 initializeApp 함수를 호출하는 부분을 이동
 document.addEventListener("DOMContentLoaded", initializeApp);
-
-// 나머지 코드는 그대로 유지
-const auth = firebase.auth();
-const database = firebase.database();
-const firestore = firebase.firestore();
-
-function renderQuickSearchPage() {
-  const root2 = document.getElementById("root2");
-  root2.innerHTML = "<p>빠른 검색 페이지 내용</p>";
-}
-
-function renderAdvancedSearchPage() {
-  const root2 = document.getElementById("root2");
-  root2.innerHTML = `<p>고급 검색 페이지 내용</p>`;
-}
-
-function renderFavoritesPage() {
-  const root2 = document.getElementById("root2");
-  root2.innerHTML = "<p>즐겨찾기 페이지 내용</p>";
-}
-
-function renderSignupPage() {
-  const root2 = document.getElementById("root2");
-  root2.innerHTML = "<p>회원가입 페이지 내용</p>";
-}
--- public/routes.js
+++ public/routes.js
@@ -1,29 +1,20 @@
-import page from './page.mjs';
-import { initializeApp } from './app.js';
+import page from "./page.mjs";
+import {initializeApp} from "./app.js";
 
-function renderQuickSearchPage() {
-  const root2 = document.getElementById("root2");
-  root2.innerHTML = "<p>빠른 검색 페이지 내용</p>";
+const pages = {
+  quickSearch: "빠른 검색 페이지 내용",
+  advancedSearch: "고급 검색 페이지 내용",
+  favorites: "즐겨찾기 페이지 내용",
+  signup: "회원가입 페이지 내용",
+};
+
+function renderPage(pageName) {
+  const root3 = document.getElementById("root2");
+  root3.innerHTML = `<p>${pages[pageName]}</p>`;
 }
 
-function renderAdvancedSearchPage() {
-  const root2 = document.getElementById("root2");
-  root2.innerHTML = "<p>고급 검색 페이지 내용</p>";
-}
-
-function renderFavoritesPage() {
-  const root2 = document.getElementById("root2");
-  root2.innerHTML = "<p>즐겨찾기 페이지 내용</p>";
-}
-
-function renderSignupPage() {
-  const root2 = document.getElementById("root2");
-  root2.innerHTML = "<p>회원가입 페이지 내용</p>";
-}
-
-
-page('/', renderQuickSearchPage);
-page('/advanced-search', renderAdvancedSearchPage);
-page('/favorites', renderFavoritesPage);
-page('/signup', renderSignupPage);
+page("/quickSearch", () => renderPage("quickSearch"));
+page("/advanced-search", () => renderPage("advancedSearch"));
+page("/favorites", () => renderPage("favorites"));
+page("/signup", () => renderPage("signup"));
 page();
