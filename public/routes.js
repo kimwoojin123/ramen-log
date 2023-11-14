64c4be5da2434dfc9469c2247448ef3957918886
kimwoojin123 <kimwoojin1903@gmail.com>
2023-11-14 16:36:15 +0900

    데이터베이스로 값들이 보내지지 않는 문제

--- public/routes.js
+++ public/routes.js
@@ -9,6 +9,54 @@ const pages = {
   </form>`,
 };
 
+// Firebase 프로젝트에 대한 인증 정보를 설정합니다.
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyBxyxuRlvxV3KsA4KErPwlZCPhbsHDN-IU",
+    authDomain: "ramen-log.firebaseapp.com",
+    projectId: "ramen-log",
+    storageBucket: "ramen-log.appspot.com",
+    messagingSenderId: "292296321273",
+    appId: "1:292296321273:web:bacc165856b927edff5a28",
+    measurementId: "G-LZHR47DK5H",
+  });
+}
+
+// 회원가입 정보를 가져오는 함수입니다.
+function getSignupInfo() {
+  // `id`와 `pw` 필드의 값을 가져옵니다.
+  const formData = new FormData(document.getElementById("signup-form"));
+  const id = formData.get("id");
+  const pw = formData.get("pw");
+  return {
+    id,
+    pw,
+  };
+}
+
+// `join()` 함수입니다.
+function join(event) {
+  // `event` 이벤트 객체를 사용합니다.
+  event.preventDefault();
+
+  // 회원가입 정보를 가져옵니다.
+  const signupInfo = getSignupInfo();
+
+  // `users` 컬렉션 내에 새로운 문서를 생성합니다.
+  const userDocRef = db.collection("users").doc("data");
+
+  // 새로운 문서에 회원가입 정보를 추가합니다.
+  userDocRef.set({
+    id: signupInfo.id,
+    pw: signupInfo.pw,
+  });
+}
+
+const signupForm = document.getElementById("signup-form");
+if (signupForm) {
+  signupForm.addEventListener("submit", join);
+}
+
 function renderPage(pageName) {
   const root2 = document.getElementById("root2");
   const root3 = document.getElementById("root3");
@@ -24,10 +72,3 @@ window.addEventListener("hashchange", () => {
   const pageName = hash.substring(1);
   renderPage(pageName);
 });
-
-// `signup` 함수를 `/signup` 경로에 연결합니다.
-const signupForm = document.getElementById("signup-form");
-if (signupForm) {
-  // `signup-form` 요소에 `submit` 이벤트 리스너를 연결합니다.
-  signupForm.addEventListener("submit", join);
-}
