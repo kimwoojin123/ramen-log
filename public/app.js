635794eb9eea3c2079c6420eca025958c44af10a
kimwoojin123 <kimwoojin1903@gmail.com>
2023-11-15 19:04:44 +0900

    테스트중

--- public/app.js
+++ public/app.js
@@ -1,1 +1,30 @@
-import "./routes.js";
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const db = getFirestore();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxyxuRlvxV3KsA4KErPwlZCPhbsHDN-IU",
+  authDomain: "ramen-log.firebaseapp.com",
+  databaseURL: "https://ramen-log-default-rtdb.firebaseio.com",
+  projectId: "ramen-log",
+  storageBucket: "ramen-log.appspot.com",
+  messagingSenderId: "292296321273",
+  appId: "1:292296321273:web:bacc165856b927edff5a28",
+  measurementId: "G-LZHR47DK5H",
+};
+const app = initializeApp(firebaseConfig);
+
+const name = document.getElementById("name");
+name.addEventListener("click", () => {
+  const aTuringRef = db.collection("users").doc("aturing");
+  aTuringRef.set(
+    {
+      first: "Alan",
+      middle: "Mathison",
+      last: "Turing",
+      born: 1912,
+    },
+    {merge: true}
+  );
+});
