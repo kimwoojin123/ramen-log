f96af7f1d37df80deeadadc2a2dd7b17f9b2b38b
kimwoojin123 <kimwoojin1903@gmail.com>
2023-11-30 11:33:23 +0900

    즐겨찾기 페이지 설정

--- public/routes.js
+++ public/routes.js
@@ -1,3 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBxyxuRlvxV3KsA4KErPwlZCPhbsHDN-IU",
+  authDomain: "ramen-log.firebaseapp.com",
+  projectId: "ramen-log",
+  storageBucket: "ramen-log.appspot.com",
+  messagingSenderId: "292296321273",
+  appId: "1:292296321273:web:bacc165856b927edff5a28",
+  measurementId: "G-LZHR47DK5H",
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore(); // Firestore 연결
+
 const pages = {
   quickSearch: "개발중 추후 구현 예정",
   advancedSearch: "개발중 추후 구현 예정",
@@ -24,27 +39,42 @@ const pages = {
 async function renderFavorites() {
   const favoritesPage = document.getElementById("favoriteStores");
 
-  // 여기서 사용자의 즐겨찾기 목록을 가져오는 로직을 구현해야 합니다.
-  // 이 곳에 가져온 즐겨찾기 목록을 favoritesPage에 추가하는 코드를 작성해야 합니다.
   try {
-    // Firebase에서 사용자의 즐겨찾기 목록을 가져오는 로직을 구현하세요.
-    // 필요한 경우 Firebase 데이터베이스나 API로부터 데이터를 가져옵니다.
-    // 가져온 데이터를 즐겨찾기 목록에 추가하는 방법에 대한 예시는 아래와 유사할 수 있습니다.
+    const currentUser = firebase.auth().currentUser;
+    if (currentUser) {
+      const userEmail = currentUser.email;
+      const userRef = db.collection("users").doc(userEmail);
 
-    // 즐겨찾기 목록을 가져왔다고 가정하고, favorites 배열에 저장합니다.
-    const favorites = ["가게 1", "가게 2", "가게 3"]; // 예시 즐겨찾기 목록
+      const userDoc = await userRef.get();
+      const userData = userDoc.data();
 
-    // favorites 배열의 각 아이템을 반복하여 목록에 추가합니다.
-    favorites.forEach((store) => {
-      const storeElement = document.createElement("p");
-      storeElement.textContent = store;
-      favoritesPage.appendChild(storeElement);
-    });
+      if (userData) {
+        // 사용자의 즐겨찾기 목록을 담을 배열
+        const favorites = [];
+
+        // userData에서 true 값인 가게명을 가져와 favorites 배열에 추가
+        for (const store in userData) {
+          if (userData[store] === true) {
+            favorites.push(store);
+          }
+        }
+
+        // favorites 배열의 각 아이템을 반복하여 목록에 추가
+        favorites.forEach((store) => {
+          const storeElement = document.createElement("p");
+          storeElement.textContent = store;
+          favoritesPage.appendChild(storeElement);
+        });
+      } else {
+        console.log("사용자의 즐겨찾기 목록이 없습니다.");
+      }
+    } else {
+      console.log("로그인되지 않은 사용자입니다.");
+    }
   } catch (error) {
     console.error("즐겨찾기 목록을 가져오는 중 에러 발생:", error);
   }
 }
-
 function renderPage(pageName) {
   const root2 = document.getElementById("root2");
   const root3 = document.getElementById("root3");
@@ -64,7 +94,14 @@ function renderPage(pageName) {
       loginForm.addEventListener("submit", loginUser);
     }
   } else if (pageName === "favorites") {
-    renderFavorites();
+    const currentUser = firebase.auth().currentUser;
+
+    if (!currentUser) {
+      alert("로그인이 필요합니다.");
+      window.location.href = "/";
+    } else {
+      renderFavorites();
+    }
   }
 }
 
